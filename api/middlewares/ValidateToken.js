fda78186016364eb0ce38d554e088d46e7a4cf0b
cristian <crissnar12xd@gmail.com>
2024-03-05 14:14:26 -0500

    config validate-2

--- api/middlewares/ValidateToken.js
+++ api/middlewares/ValidateToken.js
@@ -3,7 +3,7 @@ import Jwt from "jsonwebtoken";
 import { TOKEN_SECRET } from "../config/Token.js";
 
 export const AuthRequired = (req, res, next) => {
-  const { token } = req.cookies;
+  const token = req.headers.authorization;;
 
   if (!token)
     return res.status(401).json({ message: "no esta autorizado el token" });
