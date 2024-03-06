f3346f91242a8dab59dfaf634fa8fd73b81360c5
cristian <crissnar12xd@gmail.com>
2024-03-06 13:35:31 -0500

    updated login

--- api/controllers/Motoviajero.controller.js
+++ api/controllers/Motoviajero.controller.js
@@ -69,7 +69,7 @@ export const LoginMotoviajero = async (req, res) => {
       return res.status(404).json({ message: "incorrect password" });
 
     const token = await createAccesToken({ id: UserFound._id });
-    res.headres.authorization("token", token);
+    res.headres("Authorization", token);
     res.json({
       id: UserFound._id,
       Email_Mv: UserFound.Email_Mv,
@@ -86,7 +86,7 @@ export const LoginMotoviajero = async (req, res) => {
 // esta funcion realiza el cierre de sesion
 export const Logout = async (req, res) => {
 
-  res.headers.authorization("token", "", {
+  res.header("Authorization", "", {
     expires: new Date(0),
   });
   return res.status(200).json({message:"logout success"});
