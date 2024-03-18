553f80afff01300f5faa79c2ba80b16b6ce02243
Cristian <crissnar12xd@gmail.com>
2024-03-18 17:56:38 -0500

    added delete pqrs

--- api/routes/PQRS.routes.js
+++ api/routes/PQRS.routes.js
@@ -1,9 +1,10 @@
-import { getAllPQRS, uploadPQRS } from "../controllers/pqrs.controller.js";
+import { getAllPQRS, uploadPQRS, deletePQRS } from "../controllers/pqrs.controller.js";
 import { Router } from "express";
 
 const router=Router();
 
 router.get('/pqrs', getAllPQRS)
 router.post('/pqrs', uploadPQRS)
+router.delete('/pqrs/:id', deletePQRS)
 
 export default router;
